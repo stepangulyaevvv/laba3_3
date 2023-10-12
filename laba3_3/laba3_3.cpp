e2f150cb1381b3bb84f9f0d449506740a57838ed
gulya <gulya@STEPAN>
2023-10-12 15:59:58 +0700

    Добавьте файлы проекта.

---
+++ laba3_3/laba3_3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    float y;
+    float x;
+    float b = 2;
+    float a = 2;
+    cout << "Введите значение x: ";
+    cin >> x;
+    if (x < 2) {
+        y = 1 + sqrt(a + abs(x));
+    }
+    else if (2<=x and x<12) {
+        y = 2 + (a * x)*(a*x) + exp(x);
+    }
+    else if (x >= 12) {
+        y = x * sqrt(1 + b * log(a*a * x));
+    }
+    cout << y;
+}
